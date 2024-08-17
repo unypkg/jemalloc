e298a4bcea3bfa233ca28d8b5ee5f2abf18149d5
michacassola <mich@casso.la>
2024-08-17 15:49:30 +0200

    Update unypkg-jemalloc-build.sh

--- unypkg-jemalloc-build.sh
+++ unypkg-jemalloc-build.sh
@@ -40,8 +40,8 @@ gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "/[0-9.]+$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "/[0-9.].*" | sed "s|/||")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
@@ -77,11 +77,13 @@ get_include_paths
 
 unset LD_RUN_PATH
 
+./autogen.sh
+
 ./configure \
     --prefix=/uny/pkg/"$pkgname"/"$pkgver"
 
 make -j"$(nproc)"
-make -j"$(nproc)" check 
+
 make -j"$(nproc)" install
 
 ####################################################
